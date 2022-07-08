52b0bf71eaa7322d321358aa67b73c1ecef2de36
Anastasiia-Ciloci <anastasiiaciloci@gmail.com>
2022-07-07 23:44:29 -0400

    separeted controllers, imported bootstrap and fontawesome

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const ctrlMain = require("../controllers/main");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// //where the router looks for the URL(render is the express function for compiling view template to send as HTML response)
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Express" }); //controller content,basic
+// });
 
+// way to get the page
+// const homepageController = (req, res) => {
+//   res.render("index", { title: "Express" });
+// };
+
+/*get home page*/
+//passing the function throgh as a callback in the route defenition
+
+router.get("/", ctrlMain.index);
+/*This code links the route to the new controller by “requiring” the controller file B
+and referencing the controller function in the second parameter of the router.get
+function*/
 module.exports = router;
