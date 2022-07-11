8f4d073cdfb4bc2eaaba54c347d005528b3c4aa5
Anastasiia-Ciloci <anastasiiaciloci@gmail.com>
2022-07-11 11:40:38 -0400

    added controllers, working fine

---
+++ server/controllers/location.js
@@ -0,0 +1,19 @@
+/*Get 'home' page */
+const homelist = (req, res) => {
+  res.render("index", { title: "Home" });
+};
+
+/*Get 'location' page */
+const locationInfo = (req, res) => {
+  res.render("index", { title: "Location Info" });
+};
+/*Get 'add review' page */
+const addReview = (req, res) => {
+  res.render("index", { title: "Add Review" });
+};
+
+module.exports = {
+  homelist,
+  locationInfo,
+  addReview,
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const ctrlLocations = require("../controllers/locations");
+const ctrlLocations = require("../controllers/location");
 const ctrlOthers = require("../controllers/others");
 
 /* GET home page. */
